f03cb8e6cc4b20406473e7630ca87d12ef5f661d
Andrew Koster <andrewkoster95@gmail.com>
2021-06-18 16:46:43 -0400

    For loops were added for all the if-else conditions, where needed. README was also updated.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # password-generator
+-This code is designed to generate a random password that can include lowercase letter, uppercase letter, numbers, and special characters. 
+-The javascript file contains a variety of variables, arrays, if-else statements, and for loops to help execute the code.
+
+
+![screenshot](./assets/pictures/website-screenshot)
+
+# link
+https://akoster15.github.io/password-generator/
+
+
--- script.js
+++ script.js
@@ -7,71 +7,139 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  console.log(password);
 }
 
+//Function to generate password.
 function generatePassword() {
   var passwordLength = prompt("How many character do you want in your password?");
-  var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", "'", ":", ",", ".", "/", "<", ">", "?"];
-  var lowercaseChar = lowercaseLetters[Math.floor(Math.random()*lowercaseLetters.length)];
-  var uppercaseChar = uppercaseLetters[Math.floor(Math.random()*uppercaseLetters.length)];
-  var numberChar = numbers[Math.floor(Math.random()*numbers.length)];
-  var specialChar = specialCharacters[Math.floor(Math.random()*specialCharacters)];
+  var lowercaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var uppercaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbersChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var specialChar = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", "'", ":", ",", ".", "/", "<", ">", "?"];
 
-
+//Checking that the password length is between 8 and 128 characters.
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Error! Your password length must be between 8 and 128 characters!");
    } else if (passwordLength >= 8 || passwordLength <= 128) {
+     //Asking four separate questions to see what user wants included in password.
       var lowercase = confirm("Do you want your password to include lowercase letters?");
       var uppercase = confirm("Dp you want your password to include uppercase letters?");
       var number = confirm("Do you want your password to include numbers?");
       var special = confirm("Do you want your password to include special characters?");
     } 
 
-    var password = '';
-
-    if (lowercase && uppercase && numbers && special) {
-      alert("test!");
+    var pwd = "";
+//Combination of if-else statements and for loops to help create the password based on the users wants.  
+    if (lowercase && uppercase && number && special) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(uppercaseChar, numbersChar, specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
     } else if (lowercase == true && uppercase == true && number == true && special == false) {
-
-    } else if (lowercase == true && uppercase == true && number == true && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(uppercaseChar, numbersChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == true && number == false && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(uppercaseChar, numbersChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == false && number == false && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = uppercaseChar;
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == true && number == true && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = uppercaseChar.concat(numbersChar, specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == false && number == true && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = numbersChar.concat(specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == false && number == false && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = specialChar;
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == false && number == false && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == true && number == false && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(uppercaseChar, specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == false && number == true && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(numbersChar, specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == true && uppercase == false && number == true && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = lowercaseChar.concat(numbersChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == true && number == false && special == true) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = uppercaseChar.concat(specialChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == true && number == false && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = uppercaseChar;
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == false && number == true && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = numbersChar;
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else if (lowercase == false && uppercase == true && number == true && special == false) {
+      for (var i = 0; i<passwordLength; i++) {
+        var comboChar = uppercaseChar.concat(numbersChar);
+        var charRand = comboChar[Math.floor(Math.random()*comboChar.length)]
+        pwd += charRand;
+      }
 
     } else {
+      //Error message if user did not say yes to at least one of the questions. 
       alert("Error! You must choose at least one type of character!");
-    }
+      //Password is returned
+    } return pwd;
   }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-return password;
